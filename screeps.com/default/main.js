4cec3e34a62bf0d5c6ee202d626c982886b7a692
Nekito-Strider <adamroyalwilson@gmail.com>
2023-09-13 23:04:16 -0600

    Screeps-Scripts setup

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -13,8 +13,10 @@ module.exports.loop = function () {
     }
 
     //tower logic
-    var tower = Game.structures.structureType['STRUCTURE_TOWER'];
-    if(tower) {
+    var tower = Game.spawns['Spawn1'].room.find('FIND_MY_STRUCTURES', {
+        filter: {structureType: STRUCTURE_TOWER}
+    });
+    if(tower.length > 0) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
