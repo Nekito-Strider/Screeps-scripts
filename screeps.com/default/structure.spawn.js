d536ceaf67b9f9007859b9bb704b027c6637b757
Nekito-Strider <adamroyalwilson@gmail.com>
2023-09-15 08:13:02 -0600

    Update structure.spawn.js

--- screeps.com/default/structure.spawn.js
+++ screeps.com/default/structure.spawn.js
@@ -2,22 +2,26 @@ var structureSpawn = {
 
     /** @param {Spawn} spawn **/
     run: function (spawn) {
-       /** BAD IDEA, WILL USE UP CPU TO CALCULATE MINING LOCATIONS EVERY LOOP. MAYBE USE A CREEP TO STORE LOCATIONS TO A MEMORY?
-       
-        var harvestPosList = [];
-        const terrain = new Room.Terrain(spawn.room.name);
-        for (var source in spawn.room.source) {
-            if (terrain.get((source.pos.x + 1), (source.pos.y)) !== TERRAIN_MASK_WALL) {
-                harvestPos.push(((source.pos.x + 1), source.pos.y));
-            }
-            if (terrain.get((source.pos.x + 1), (source.pos.y - 1)) !== TERRAIN_MASK_WALL) {
-                harvestPos.push(((source.pos.x + 1), (source.pos.y - 1)));
-            }
-            if (terrain.get((source.pos.x), (source.pos.y - 1)) !== TERRAIN_MASK_WALL) {
-                harvestPos.push(((source.pos.x), (source.pos.y - 1)));
+        var spawnRoom = Game.rooms.sim; //edit for server
+        var spawnSources = spawn.room.find(FIND_SOURCES);
+        var harvestPosList = []
+        // BAD IDEA, WILL USE UP CPU TO CALCULATE MINING LOCATIONS EVERY LOOP. MAYBE USE A CREEP TO STORE LOCATIONS TO A MEMORY?
+        //const terrain = new Room.Terrain(spawn.room.name);
+        if (spawn.room.memory(!harvestPosList)) {
+            for (var source in spawnSources) {
+                if (spawnRoom.terrain.get((source.pos.x + 1), (source.pos.y)) !== TERRAIN_MASK_WALL) {
+                    harvestPosList.push(((source.pos.x + 1), source.pos.y));
+                }
+                if (spawnRoom.terrain.get((source.pos.x + 1), (source.pos.y - 1)) !== TERRAIN_MASK_WALL) {
+                    harvestPosList.push(((source.pos.x + 1), (source.pos.y - 1)));
+                }
+                if (spawnRoom.terrain.get((source.pos.x), (source.pos.y - 1)) !== TERRAIN_MASK_WALL) {
+                    harvestPosList.push(((source.pos.x), (source.pos.y - 1)));
+                }
+                spawn.room.memory.harvestPosList = harvestPosList
             }
         }
-        console.log(harvestPosList[0]);*/
+        console.log(harvestPosList[0]);
     }
 };
 
